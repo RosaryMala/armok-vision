64eaad5e9096ad4d29c03c922e798fe8ca8cc637
JapaMala <japa.mala.illo@gmail.com>
2014-06-13 11:40:00 +0530

    Initial merge

---
+++ Assets/Layered Sprite/CameraFacing.cs
@@ -0,0 +1,53 @@
+﻿//	CameraFacing.cs 
+//	original by Neil Carter (NCarter)
+//	modified by Hayden Scott-Baron (Dock) - http://starfruitgames.com
+//  allows specified orientation axis
+
+
+using UnityEngine;
+using System.Collections;
+
+public class CameraFacing : MonoBehaviour
+{
+    Camera referenceCamera;
+
+    public enum Axis { up, down, left, right, forward, back };
+    public bool reverseFace = false;
+    public Axis axis = Axis.up;
+
+    // return a direction based upon chosen axis
+    public Vector3 GetAxis(Axis refAxis)
+    {
+        switch (refAxis)
+        {
+            case Axis.down:
+                return Vector3.down;
+            case Axis.forward:
+                return Vector3.forward;
+            case Axis.back:
+                return Vector3.back;
+            case Axis.left:
+                return Vector3.left;
+            case Axis.right:
+                return Vector3.right;
+        }
+
+        // default is Vector3.up
+        return Vector3.up;
+    }
+
+    void Awake()
+    {
+        // if no camera referenced, grab the main camera
+        if (!referenceCamera)
+            referenceCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        // rotates the object relative to the camera
+        Vector3 targetPos = transform.position + referenceCamera.transform.rotation * (reverseFace ? Vector3.forward : Vector3.back);
+        Vector3 targetOrientation = referenceCamera.transform.rotation * GetAxis(axis);
+        transform.LookAt(targetPos, targetOrientation);
+    }
+}
---
+++ Assets/World Scripts/site.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class site : MonoBehaviour
+{
+
+    public int blocks_x = 1;
+    public int blocks_y = 1;
+    public int blocks_z = 1;
+    int old_blocks_x = -1;
+    int old_blocks_y = -1;
+    int old_blocks_z = -1;
+
+    int[] tiletypes;
+
+    static int default_tile_type = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //this is called whenever a variable is changed from the editor.
+    void OnValidate()
+    {
+        if (blocks_x < 1) blocks_x = 1;
+        if (blocks_y < 1) blocks_y = 1;
+        if (blocks_z < 1) blocks_z = 1;
+        VerifyListSizes();
+    }
+
+    int CoordsToIndex(int inX, int inY, int inZ)
+    {
+        if (
+            (inX < blocks_x * 16) &&
+            (inY < blocks_y * 16) &&
+            (inZ < blocks_z) &&
+            (inX >= 0) &&
+            (inY >= 0) &&
+            (inZ >= 0)
+            )
+            return (inX + (inY * blocks_y * 16) + (inZ * blocks_x * 16 * blocks_y * 16));
+        else
+            return -1;
+    }
+    int OldCoordsToIndex(int inX, int inY, int inZ)
+    {
+        if (
+            (inX < old_blocks_x * 16) &&
+            (inY < old_blocks_y * 16) &&
+            (inZ < old_blocks_z) &&
+            (inX >= 0) &&
+            (inY >= 0) &&
+            (inZ >= 0)
+            )
+            return (inX + (inY * old_blocks_y * 16) + (inZ * old_blocks_x * 16 * old_blocks_y * 16));
+        else
+            return -1;
+    }
+
+    void VerifyListSizes()
+    {
+        if ((old_blocks_x != blocks_x) || (old_blocks_y != blocks_y) || (old_blocks_z != blocks_z))
+        {
+            int[] newTiletypes = new int[blocks_x * 16 * blocks_y * 16 * blocks_z];
+            for (int cur_z = 0; cur_z < blocks_z; cur_z++)
+                for (int cur_y = 0; cur_y < blocks_y * 16; cur_y++)
+                    for (int cur_x = 0; cur_x < blocks_x * 16; cur_x++)
+                    {
+                        int newIndex = CoordsToIndex(cur_x, cur_y, cur_z);
+                        int oldIndex = OldCoordsToIndex(cur_x, cur_y, cur_z);
+                        if (oldIndex >= 0)
+                            newTiletypes[newIndex] = tiletypes[oldIndex];
+                        else
+                            newTiletypes[newIndex] = default_tile_type;
+                    }
+            tiletypes = newTiletypes;
+            old_blocks_x = blocks_x;
+            old_blocks_y = blocks_y;
+            old_blocks_z = blocks_z;
+        }
+    }
+}
