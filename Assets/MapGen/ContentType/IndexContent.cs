e7142fe199ee7d66bb01018d00b61908185b2f9f
Japa <japa.mala.illo@mail.com>
2015-02-05 19:05:29 +0530

    Completely revamped the material loading system, making room for expansions later. Load up far away blocks slowly, over time.
    Signed-off-by: Japa <japa.mala.illo@mail.com>

---
+++ Assets/MapGen/ContentType/IndexContent.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Linq;
+
+public class IndexContent : IContent
+{
+    public int value;
+    public bool AddTypeElement(System.Xml.Linq.XElement elemtype)
+    {
+        XAttribute indexAtt = elemtype.Attribute("index");
+        if (indexAtt == null)
+        {
+            //Add error message here
+            value = default(int);
+            return false;
+        }
+        return int.TryParse(indexAtt.Value, out value);
+    }
+}
