34785e3fe33cb1362374327b6132a32ffdd82501
Japa <japa.mala.illo@mail.com>
2015-02-08 23:12:55 +0530

    Added the ability to effect the feature size and density of random variation.

--- Assets/MapGen/MultiMatcher/RandomConfiguration.cs
+++ Assets/MapGen/MultiMatcher/RandomConfiguration.cs
@@ -1,6 +1,4 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Xml.Linq;
 
 public class RandomConfiguration<T> : ContentConfiguration<T> where T : IContent, new()
@@ -9,6 +7,10 @@ public class RandomConfiguration<T> : ContentConfiguration<T> where T : IContent
     {
         public Content content;
         public OpenSimplexNoise noise;
+        public float scale_x = 1;
+        public float scale_y = 1;
+        public float scale_z = 1;
+        public float intensity = 1;
     }
     List<RandomItem> items = new List<RandomItem>();
 
@@ -18,7 +20,9 @@ public class RandomConfiguration<T> : ContentConfiguration<T> where T : IContent
         Content maxContent = null;
         foreach (var item in items)
         {
-            double curValue = item.noise.eval(tile.position.x, tile.position.y, tile.position.z);
+            double curValue = (
+                item.noise.eval(tile.position.x / item.scale_x, tile.position.y / item.scale_y, tile.position.z / item.scale_z)
+                + 0.944824004155211) * item.intensity; // -0.944824004155211 was found on testing to be the minimum.
             if(curValue > maxValue)
             {
                 maxValue = curValue;
@@ -42,6 +46,43 @@ public class RandomConfiguration<T> : ContentConfiguration<T> where T : IContent
             //right now, we don't actuallyu care about any parameters
             int seed = items.Count;
             RandomItem item = new RandomItem();
+            XAttribute elemScale = elemRandom.Attribute("scale");
+            if (elemScale != null)
+            {
+                float scale = 1;
+                float.TryParse(elemScale.Value, out scale);
+                item.scale_x = scale;
+                item.scale_y = scale;
+                item.scale_z = scale;
+            }
+            XAttribute elemScaleX = elemRandom.Attribute("scale_x");
+            if (elemScaleX != null)
+            {
+                float scale = 1;
+                float.TryParse(elemScaleX.Value, out scale);
+                item.scale_x = scale;
+            }
+            XAttribute elemScaleY = elemRandom.Attribute("scale_y");
+            if (elemScaleY != null)
+            {
+                float scale = 1;
+                float.TryParse(elemScaleY.Value, out scale);
+                item.scale_y = scale;
+            }
+            XAttribute elemScaleZ = elemRandom.Attribute("scale_z");
+            if (elemScaleZ != null)
+            {
+                float scale = 1;
+                float.TryParse(elemScaleZ.Value, out scale);
+                item.scale_z = scale;
+            }
+            XAttribute elemIntensity = elemRandom.Attribute("intensity");
+            if (elemIntensity != null)
+            {
+                float scale = 1;
+                float.TryParse(elemIntensity.Value, out scale);
+                item.intensity = scale;
+            }
             item.content = content;
             item.noise = new OpenSimplexNoise(seed);
             items.Add(item);
