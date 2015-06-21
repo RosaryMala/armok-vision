3396d64cee38eba6af6817010f62a7c9002e313d
Japa <japa.mala.illo@mail.com>
2015-06-21 09:31:53 +0530

    Moved some things around, and added an item matcher.

---
+++ Assets/MapGen/MultiMatcher/ItemMatcher.cs
@@ -0,0 +1,82 @@
+﻿using RemoteFortressReader;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+static class ItemTokenList
+{
+    static List<MaterialDefinition> _itemTokenList;
+    public static List<MaterialDefinition> itemTokenList
+    {
+        set
+        {
+            if (_itemTokenList != value)
+            {
+                _itemTokenList = value;
+                PopulateWordLists();
+            }
+        }
+    }
+    public static Dictionary<string, MaterialDefinition> itemLookup { get; private set; }
+
+    static void PopulateWordLists()
+    {
+        foreach (MaterialDefinition token in _itemTokenList)
+        {
+            if (itemLookup == null)
+                itemLookup = new Dictionary<string, MaterialDefinition>();
+
+            itemLookup[token.id] = token;
+        }
+    }
+
+}
+
+
+public class ItemMatcher<T>
+{
+    Dictionary<MatPairStruct, T> itemList;
+    public T this[string token]
+    {
+        set
+        {
+            if (itemList == null)
+                itemList = new Dictionary<MatPairStruct, T>();
+            itemList[ItemTokenList.itemLookup[token].mat_pair] = value;
+        }
+    }
+    public T this[MatPairStruct mat]
+    {
+        set
+        {
+            if (itemList == null)
+                itemList = new Dictionary<MatPairStruct, T>();
+            itemList[mat] = value;
+        }
+    }
+    public bool Get(MatPairStruct mat, out T value)
+    {
+        if(itemList != null)
+        {
+            T output;
+            if (itemList.TryGetValue(mat, out output))
+            {
+                value = output;
+                return true;
+            }
+            mat.mat_type = -1; //Try once more with a more generic value.
+            if (itemList.TryGetValue(mat, out output))
+            {
+                value = output;
+                return true;
+            }
+
+        }
+        value = default(T);
+        return false;
+    }
+    public void Clear()
+    {
+        itemList.Clear();
+    }
+}
