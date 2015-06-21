95b1d9a787ae9353ee9f37ea87c94b73138ff158
Japa <japa.mala.illo@gmail.com>
2015-06-21 09:31:53 +0530

    Moved some things around, and added an item matcher.

---
+++ Assets/MapGen/ContentChosers/ItemConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Linq;
+
+public class ItemContent<T> : ContentConfiguration<T> where T : IContent, new()
+{
+    public override bool GetValue(MapDataStore.Tile tile, MeshLayer layer, out T value)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void ParseElementConditions(XElement elemtype, ContentConfiguration<T>.Content content)
+    {
+        throw new System.NotImplementedException();
+    }
+}
