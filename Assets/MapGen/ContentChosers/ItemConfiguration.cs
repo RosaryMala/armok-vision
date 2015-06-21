a04324d11f6b5bb8ee39b6023b57618a8d4bd284
Japa <japa.mala.illo@gmail.com>
2015-06-21 11:59:21 +0530

    Differentiate between walls made from bars, blocks, and wood.

--- Assets/MapGen/ContentChosers/ItemConfiguration.cs
+++ Assets/MapGen/ContentChosers/ItemConfiguration.cs
@@ -1,14 +1,35 @@
 ﻿using System.Xml.Linq;
 
-public class ItemContent<T> : ContentConfiguration<T> where T : IContent, new()
+public class ItemConfiguration<T> : ContentConfiguration<T> where T : IContent, new()
 {
+    ItemMatcher<Content> itemMatcher = new ItemMatcher<Content>();
+
     public override bool GetValue(MapDataStore.Tile tile, MeshLayer layer, out T value)
     {
-        throw new System.NotImplementedException();
+        Content cont;
+        if (itemMatcher.Get(tile.construction_item, out cont))
+        {
+            value = cont.GetValue(tile, layer);
+            return true;
+        }
+        value = default(T);
+        return false;
     }
 
     protected override void ParseElementConditions(XElement elemtype, ContentConfiguration<T>.Content content)
     {
-        throw new System.NotImplementedException();
+        var elemItems = elemtype.Elements("item");
+        foreach (XElement elemItem in elemItems)
+        {
+            XAttribute elemToken = elemItem.Attribute("token");
+            if (elemToken != null)
+            {
+                if (elemToken.Value == "NONE")
+                    defaultItem = content;
+                else
+                    itemMatcher[elemToken.Value] = content;
+                continue;
+            }
+        }
     }
 }
