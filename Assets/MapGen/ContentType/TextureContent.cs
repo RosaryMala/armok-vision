19315a4f3356484c4738e43ce1e7e0fcfab14c70
Japa <japa.mala.illo@mail.com>
2015-06-28 22:55:57 +0530

    Almost got the texture atlas working. Everything is broken though.

--- Assets/MapGen/ContentType/TextureContent.cs
+++ Assets/MapGen/ContentType/TextureContent.cs
@@ -1,15 +1,66 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Xml.Linq;
+using UnityEngine;
+using System.IO;
+using System;
+using UnityExtension;
 
-public class TextureContent : IContent {
+public class TextureContent : IContent
+{
+
+    TextureStorage store;
+    int storageIndex;
+
+    public Matrix4x4 UVTransform
+    {
+        get
+        {
+            return store.getUVTransform(storageIndex);
+        }
+    }
 
     public bool AddTypeElement(System.Xml.Linq.XElement elemtype)
     {
-        throw new System.NotImplementedException();
+        if (store == null) //nowhere to put the image.
+        {
+            Debug.LogError("Texture Storage is Null: " + elemtype);
+            return false;
+        }
+
+        XAttribute fileAtt = elemtype.Attribute("file");
+        if (fileAtt == null)
+        {
+            Debug.LogError("No file attribute in " + elemtype);
+            //Add error message here
+            return false;
+        }
+        string filePath = Path.Combine(Path.GetDirectoryName(new Uri(elemtype.BaseUri).LocalPath), fileAtt.Value);
+        filePath = Path.GetFullPath(filePath);
+
+        if (!File.Exists(filePath))
+        {
+            Debug.LogError("File not found: " + filePath);
+            return false;
+        }
+
+        byte[] fileData = File.ReadAllBytes(filePath);
+
+        Texture2D tex = new Texture2D(2,2);
+        tex.LoadImage(fileData);
+
+        tex.name = filePath;
+
+        storageIndex = store.AddTexture(tex);
+        Debug.Log("Added texture " + filePath);
+        return true;
     }
 
     public object ExternalStorage
     {
-        set { throw new System.NotImplementedException(); }
+        set
+        {
+            store = value as TextureStorage;
+            if (store == null)
+                Debug.LogError(value + "Is null, for some reason");
+        }
     }
 }
