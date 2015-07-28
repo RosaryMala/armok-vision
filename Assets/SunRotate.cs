ec6689a4f38afafb4436a9d0bb7d7e7ab730d467
Japa <japa.mala.illo@gmail.com>
2015-07-28 20:02:30 +0530

    Added (Currently very fast) sun movement.
    Does not follow DF time, and does not have any light at all at night.

---
+++ Assets/SunRotate.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunRotate : MonoBehaviour {
+    public float rotationSpeed = 1.0f;
+
+    Light currentLight;
+
+    void Awake()
+    {
+        currentLight = GetComponent<Light>();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 rotation = SunAngle.CalculateSunPosition((int)(Time.time * rotationSpeed), 0, 0);
+        float brightness = rotation.x + 1;
+        brightness /= 2.0f;
+        if (brightness < 0)
+            currentLight.intensity = 0;
+        else if (brightness > 1)
+            currentLight.intensity = 1;
+        else
+            currentLight.intensity = brightness;
+        Quaternion rotate = Quaternion.Euler(rotation);
+        transform.rotation = rotate;
+	}
+}
