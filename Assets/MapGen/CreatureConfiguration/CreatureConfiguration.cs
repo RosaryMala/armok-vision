ba33979bc2686727c72421b6632dc937b1ba1cd6
Japa <japa.mala.illo@gmail.com>
2015-12-18 20:55:06 +0530

    initial work on setting things for creatures by race. Not nearly finished yet.

--- Assets/MapGen/CreatureConfiguration/CreatureConfiguration.cs
+++ Assets/MapGen/CreatureConfiguration/CreatureConfiguration.cs
@@ -1,5 +1,90 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Linq;
+using System.Xml.Linq;
+using UnityEngine;
+using RemoteFortressReader;
 
-public class CreatureConfiguration {
+abstract public class CreatureConfiguration<T> where T : IContent, new()
+{
+    protected class Content
+    {
+        public T defaultItem { get; set; }
+        public CreatureConfiguration<T> overloadedItem { get; set; }
+        public T GetValue(UnitDefinition unit)
+        {
+            if (overloadedItem == null)
+                return defaultItem;
+            else
+            {
+                T item;
+                if (overloadedItem.GetValue(unit, out item))
+                {
+                    return item;
+                }
+                else
+                    return defaultItem;
+            }
+        }
+    }
+
+    abstract public bool GetValue(UnitDefinition unit, out T value);
+
+    abstract protected void ParseElementConditions(XElement elemtype, Content content);
+
+    abstract public object SecondaryDictionary { set; }
+
+    string nodeName { get; set; }
+
+    void ParseContentElement(XElement elemtype, object externalStorage, object secondaryDictionary)
+    {
+        T value = new T();
+        value.ExternalStorage = externalStorage;
+        if (!value.AddTypeElement(elemtype))
+        {
+            Debug.LogError("Couldn't parse " + elemtype);
+            //There was an error parsing the type
+            //There's nothing to work with.
+            return;
+        }
+        value.ExternalStorage = externalStorage;
+        Content content = new Content();
+        content.defaultItem = value;
+        ParseElementConditions(elemtype, content);
+        if (elemtype.Element("subObject") != null)
+        {
+            content.overloadedItem = GetFromRootElement(elemtype, "subObject");
+            content.overloadedItem.AddSingleContentConfig(elemtype, externalStorage, secondaryDictionary);
+        }
+    }
+
+    public bool AddSingleContentConfig(XElement elemRoot, object externalStorage = null, object secondaryDictionary = null)
+    {
+        SecondaryDictionary = secondaryDictionary;
+        var elemValues = elemRoot.Elements(nodeName);
+        foreach (XElement elemValue in elemValues)
+        {
+            ParseContentElement(elemValue, externalStorage, secondaryDictionary);
+        }
+        return true;
+    }
+
+    public static CreatureConfiguration<T> GetFromRootElement(XElement elemRoot, XName name)
+    {
+        CreatureConfiguration<T> output;
+        if (elemRoot.Element(name).Elements().Count() == 0)
+        {
+            output = new CreatureRaceConfiguration<T>();
+            output.nodeName = name.LocalName;
+            return output;
+        }
+        switch (elemRoot.Element(name).Elements().First().Name.LocalName)
+        {
+            default:
+                output = new CreatureRaceConfiguration<T>();
+                break;
+        }
+        output.nodeName = name.LocalName;
+        return output;
+    }
+
+
 }
---
+++ Assets/MapGen/CreatureConfiguration/CreatureRaceConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using RemoteFortressReader;
+using System;
+using System.Xml.Linq;
+
+public class CreatureRaceConfiguration<T> : CreatureConfiguration<T> where T : IContent, new()
+{
+    CreatureRaceMatcher<Content> raceStorage = new CreatureRaceMatcher<Content>();
+    public override object SecondaryDictionary
+    {
+        set
+        {
+        }
+    }
+
+    public override bool GetValue(UnitDefinition unit, out T value)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void ParseElementConditions(XElement elemtype, Content content)
+    {
+        throw new NotImplementedException();
+    }
+}
