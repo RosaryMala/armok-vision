a59016af64eedece2192111f3859815281b23bee
Japa <japa.mala.illo@mail.com>
2016-01-27 18:29:09 +0530

    Started work on a spritecontent class, but it's far from finished.

---
+++ Assets/MapGen/ContentType/SpriteContent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Xml.Linq;
+using UnityEngine;
+
+public class SpriteContent : IContent
+{
+    TextureStorage store;
+    int storageIndex;
+
+    public Matrix4x4 UVTransform
+    {
+        get
+        {
+            return store.getUVTransform(storageIndex);
+        }
+    }
+
+    public object ExternalStorage
+    {
+        set
+        {
+            store = value as TextureStorage;
+            if (store == null)
+                Debug.LogError(value + "Is null, for some reason");
+        }
+    }
+
+    public bool AddTypeElement(XElement elemtype)
+    {
+        throw new NotImplementedException();
+    }
+}
