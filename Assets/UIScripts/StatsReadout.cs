65d01fe233ec7af66fff450d67b58901588d9217
Japa <japa.mala.illo@mail.com>
2016-02-04 00:19:58 +0530

    Added a config storage system, with currently very little actually in it. To be expanded later.
    Made the mesh queue not accept mesh processing requests once it's full.
    
    Added some status text back.

---
+++ Assets/UIScripts/StatsReadout.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// A collection of whatever status numbers we might need. It should be writable from any thread.
+/// </summary>
+[RequireComponent(typeof(Text))]
+public class StatsReadout : MonoBehaviour
+{
+    private static int _blocksDrawn;
+    public static int BlocksDrawn
+    {
+        get { return _blocksDrawn; }
+        set { Interlocked.Exchange(ref _blocksDrawn, value); }
+    }
+    private static int _queueLength;
+    public static int QueueLength
+    {
+        get { return _queueLength; }
+        set { Interlocked.Exchange(ref _queueLength, value); }
+    }
+    private static long _blockProcessTime;
+    public static TimeSpan BlockProcessTime
+    {
+        get { return new TimeSpan(_blockProcessTime); }
+        set { Interlocked.Exchange(ref _blockProcessTime, value.Ticks); }
+    }
+
+
+
+    private Text text;
+
+    public void Awake()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // LateUpdate is called every frame, if the Behaviour is enabled
+    public void LateUpdate()
+    {
+        StringBuilder updateBuilder = new StringBuilder();
+        updateBuilder.Append("Blocks drawn: ").Append(BlocksDrawn).AppendLine();
+        updateBuilder.Append("Blocks waiting to be processed: ").Append(QueueLength).AppendLine();
+        updateBuilder.Append("Block processing time: ").Append(BlockProcessTime.Milliseconds).Append("ms (").Append((Time.deltaTime / BlockProcessTime.TotalSeconds).ToString("F2")).Append(" blocks per frame)").AppendLine();
+        text.text = updateBuilder.ToString();
+    }
+}
