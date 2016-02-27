a99c905101f022aafa13fdcae1a6ac4309a5a0a5
Japa <japa.mala.illo@gmail.com>
2016-02-27 22:13:13 +0530

    Made the sun angle change with the seasons. DF is assumed to be in central Europe for this purpose.

--- Assets/CelestialScripts/SunRotate.cs
+++ Assets/CelestialScripts/SunRotate.cs
@@ -3,11 +3,39 @@
 public class SunRotate : MonoBehaviour
 {
     public float rotationSpeed = 1.0f;
+    public float longitude;
+    public float axialTilt;
+
+    public Transform longitudeAxis;
+    public Transform seasonAxis;
+
+    public float GetLongitudeFromWorld(RemoteFortressReader.WorldMap world)
+    {
+        float tude = Mathf.InverseLerp(0, world.world_height, world.center_y);
+        switch (world.world_poles)
+        {
+            case RemoteFortressReader.WorldPoles.NO_POLES:
+                return 0;
+            case RemoteFortressReader.WorldPoles.NORTH_POLE:
+                tude = (1-tude) * 90;
+                    break;
+            case RemoteFortressReader.WorldPoles.SOUTH_POLE:
+                tude = tude * 90;
+                break;
+            case RemoteFortressReader.WorldPoles.BOTH_POLES:
+                tude = ((tude * 2) - 1) * -180;
+                break;
+            default:
+                return 0;
+        }
+        return tude;
+    }
 
     // Update is called once per frame
     void Update()
     {
         Quaternion rotate = Quaternion.AngleAxis(TimeHolder.DisplayedTime.SunAngle, Vector3.back);
         transform.localRotation = rotate;
+        seasonAxis.localRotation = Quaternion.Euler((Mathf.Cos(TimeHolder.DisplayedTime.SolsticeAngle * Mathf.Deg2Rad) * axialTilt) + 90, 0, 0);
     }
 }
