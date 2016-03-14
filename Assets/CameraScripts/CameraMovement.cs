5174693c258977b128a509d80eb12a678e0cd32b
Ben Lubar <ben.lubar@gmail.com>
2016-03-14 14:12:17 -0500

    double follow speed

--- Assets/CameraScripts/CameraMovement.cs
+++ Assets/CameraScripts/CameraMovement.cs
@@ -7,7 +7,7 @@ public class CameraMovement : MonoBehaviour
     GameMap gameMap;
     public Transform cameraPos;
     bool following = true;
-    public float followSpeed = 0.25f;
+    public float followSpeed = 0.5f;
     public float followSnapDistance = 10;
     public float speed = 10;
     public float minDistance = 5;
