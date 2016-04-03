7dd32af4ce54966ebf930be36789bfc7682795cc
Japa <japa.mala.illo@mail.com>
2016-04-03 11:11:59 +0530

    Added ability to stream the DF screen to AV

---
+++ Assets/PosTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PosTest : MonoBehaviour
+{
+    public UnityEngine.UI.Text posText;
+
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 pos = transform.localPosition;
+        posText.text = "Pos: " + pos.z.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.localPosition;
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            pos.z += 0.1f;
+            posText.text = "Pos: " + pos.z.ToString();
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            pos.z -= 0.1f;
+            posText.text = "Pos: " + pos.z.ToString();
+        }
+        transform.localPosition = pos;
+    }
+}
---
+++ Assets/SizeTest.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SizeTest : MonoBehaviour
+{
+
+    float size = 1.0f;
+    public UnityEngine.UI.Text sizetext;
+
+    // Use this for initialization
+    void Start()
+    {
+        sizetext.text = "Size: " + size.ToString();
+        transform.localScale = new Vector3(size, size, size);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            size += 0.1f;
+            transform.localScale = new Vector3(size, size, size);
+            sizetext.text = "Size: " + size.ToString();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            size -= 0.1f;
+            transform.localScale = new Vector3(size, size, size);
+            sizetext.text = "Size: " + size.ToString();
+        }
+    }
+}
