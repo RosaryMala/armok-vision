0a2b3b28ebdfadc186ff4ca79c3f4ce64f538fe5
Japa <japa.mala.illo@gmail.com>
2016-08-05 00:38:10 +0530

    Add trees to the world map, and correct the colors.

---
+++ Assets/MapGen/Vector2Extension.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public static class Vector2Extension
+{
+
+    public static Vector2 Rotate(this Vector2 v, float degrees)
+    {
+        float sin = Mathf.Sin(degrees * Mathf.Deg2Rad);
+        float cos = Mathf.Cos(degrees * Mathf.Deg2Rad);
+
+        float tx = v.x;
+        float ty = v.y;
+        v.x = (cos * tx) - (sin * ty);
+        v.y = (sin * tx) + (cos * ty);
+        return v;
+    }
+}
