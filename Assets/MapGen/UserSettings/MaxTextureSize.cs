48cb2cea9988f29cb5906ff296ec3031c06776b8
Japa <japa.mala.illo@gmail.com>
2016-08-17 23:57:52 +0530

    Add sliders for max texture size and texture atlas size

---
+++ Assets/MapGen/UserSettings/MaxTextureSize.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace UserSettings
+{
+    public class MaxTextureSize : SliderBase
+    {
+        protected override void InitValue()
+        {
+            slider.value = Mathf.Log(GameSettings.Instance.rendering.maxTextureSize) / Mathf.Log(2);
+            valueLabel.text = GameSettings.Instance.rendering.maxTextureSize.ToString();
+        }
+
+        protected override void OnValueChanged(float value)
+        {
+            GameSettings.Instance.rendering.maxTextureSize = (int)Mathf.Pow(2, (int)value); //converting value to int to make sure it's a pure power of 2
+            valueLabel.text = GameSettings.Instance.rendering.maxTextureSize.ToString();
+        }
+    }
+}
---
+++ Assets/MapGen/UserSettings/TextureAtlasSize.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace UserSettings
+{
+    public class TextureAtlasSize : SliderBase
+    {
+        protected override void InitValue()
+        {
+            slider.value = Mathf.Log(GameSettings.Instance.rendering.textureAtlasSize) / Mathf.Log(2);
+            valueLabel.text = GameSettings.Instance.rendering.textureAtlasSize.ToString();
+        }
+
+        protected override void OnValueChanged(float value)
+        {
+            GameSettings.Instance.rendering.textureAtlasSize = (int)Mathf.Pow(2, (int)value); //converting value to int to make sure it's a pure power of 2
+            valueLabel.text = GameSettings.Instance.rendering.textureAtlasSize.ToString();
+        }
+    }
+}
