fce761638cbb329ac446f4c55c3c637b59d034e7
Japa <japa.mala.illo@gmail.com>
2016-08-24 23:36:37 +0530

    Made the toggles for clouds and distant terrain have an instant effect for turning off.

--- Assets/MapGen/UserSettings/DrawDistantTerrain.cs
+++ Assets/MapGen/UserSettings/DrawDistantTerrain.cs
@@ -4,16 +4,21 @@ namespace UserSettings
 {
     public class DrawDistantTerrain : SliderBase
     {
+        WorldMapMaker worldMap;
+
         protected override void InitValue()
         {
             slider.value = (int)GameSettings.Instance.rendering.distantTerrainDetail;
             valueLabel.text = GameSettings.Instance.rendering.distantTerrainDetail.ToString();
+            worldMap = FindObjectOfType<WorldMapMaker>();
         }
 
         protected override void OnValueChanged(float value)
         {
             GameSettings.Instance.rendering.distantTerrainDetail = (GameSettings.LandscapeDetail)value;
             valueLabel.text = GameSettings.Instance.rendering.distantTerrainDetail.ToString();
+            var regions = FindObjectsOfType<RegionMaker>();
+            worldMap.gameObject.SetActive(GameSettings.Instance.rendering.distantTerrainDetail != GameSettings.LandscapeDetail.Off);
         }
     }
 }
