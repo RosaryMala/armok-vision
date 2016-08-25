f2a6bd4d2b9307d61d05214649b384edcaf7ea1c
Japa <japa.mala.illo@gmail.com>
2016-08-25 13:48:12 +0530

    Made the auto build script generate a manifest.json file.

---
+++ Assets/BuildSettings.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class BuildSettings : ScriptableObject {
+    public string author;
+    public string content_version;
+    public string df_min_version;
+    public string df_max_version;
+    public string title;
+    public string tooltip;
+    public string win_exe;
+    public string osx_exe;
+    public string linux_exe;
+    public bool launch_with_terminal;
+    public string readme;
+    public bool needs_dfhack;
+}
