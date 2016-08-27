10e280d632f7cb5f486ff843623b5b972e8abe6a
Japa <japa.mala.illo@gmail.com>
2016-08-27 23:17:56 +0530

    Preliminary attachment of the VR rig onto the existing camera setup.

--- Assets/CameraScripts/CameraScale.cs
+++ Assets/CameraScripts/CameraScale.cs
@@ -17,6 +17,7 @@ public class CameraScale : MonoBehaviour
     CameraRotate cameraRotate;
     CameraMovement cameraMovement;
     CameraRotateVertical cameraRotateVertical;
+    public Camera steamVrCamera;
 
     void SetFirstPerson(bool value)
     {
@@ -57,6 +58,16 @@ public class CameraScale : MonoBehaviour
     public void Start()
     {
         SetFirstPerson(false);
+        if (SteamVR.active)
+        {
+            camera.gameObject.SetActive(false);
+            steamVrCamera.gameObject.SetActive(true);
+        }
+        else
+        {
+            camera.gameObject.SetActive(true);
+            steamVrCamera.gameObject.SetActive(false);
+        }
     }
 
     private void HandleMouseRotation()
