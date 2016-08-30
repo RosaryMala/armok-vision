5be5b8eddcaff1b7be363fbc4db54ae45a02ee31
Japa <japa.mala.illo@gmail.com>
2016-08-31 00:08:35 +0530

    Made deferred rendering a user-selectable option.

---
+++ Assets/MapGen/UserSettings/DeferredRendering.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace UserSettings
+{
+    public class DeferredRendering : SliderBase
+    {
+        protected override void InitValue()
+        {
+            slider.value = GameSettings.Instance.rendering.deferredRendering;
+            OnValueChanged(slider.value);
+        }
+
+        protected override void OnValueChanged(float value)
+        {
+            GameSettings.Instance.rendering.deferredRendering = (int)value;
+            switch (GameSettings.Instance.rendering.deferredRendering)
+            {
+                case 2:
+                    cam.renderingPath = UnityEngine.RenderingPath.DeferredShading;
+                    valueLabel.text = "Deferred";
+                    break;
+                case 1:
+                    cam.renderingPath = UnityEngine.RenderingPath.Forward;
+                    valueLabel.text = "Forward";
+                    break;
+                case 0:
+                    cam.renderingPath = UnityEngine.RenderingPath.VertexLit;
+                    valueLabel.text = "Vertex Lit";
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
