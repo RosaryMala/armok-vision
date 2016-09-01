b289b046f77153e65f051579c16839b5dfc97916
Japa <japa.mala.illo@gmail.com>
2016-09-01 19:46:20 +0530

    Added a camera selector, and a very temporary third person camera.

---
+++ Assets/CameraScripts/MapPositionUpdater.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapPositionUpdater : MonoBehaviour
+{
+    GameMap map;
+
+    public Vector3 offset;
+
+    public void Awake()
+    {
+        map = FindObjectOfType<GameMap>();
+    }
+
+    Vector3 oldPos;
+
+    public void Update()
+    {
+        Vector3 newPos = transform.TransformPoint(offset);
+
+        if (map != null && (oldPos - newPos).sqrMagnitude > 0.01)
+        {
+            map.UpdateCenter(newPos);
+            oldPos = newPos;
+        }
+    }
+}
