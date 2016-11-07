d7b0ba8f17790a4f60633f51848868d9ccf252b3
Japa <japa.mala.illo@gmail.com>
2016-11-07 21:57:31 +0530

    Added a play/pause button, and improved the clock ui.

---
+++ Assets/Ui/PanelSlider.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelSlider : MonoBehaviour
+{
+    private Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public bool isOut = true;
+
+    public void Slide()
+    {
+        if (isOut)
+        {
+            anim.Play("PanelSlideIn");
+            isOut = false;
+        }
+        else
+        {
+            anim.Play("PanelSlideOut");
+            isOut = true;
+        }
+    }
+}
---
+++ Assets/Ui/PlayPauseManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayPauseManager : MonoBehaviour
+{
+
+    public GameObject playButton;
+    public GameObject pauseButton;
+
+    bool paused = false;
+
+    void Start()
+    {
+        playButton.SetActive(paused);
+        pauseButton.SetActive(!paused);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (paused != DFConnection.Instance.DfPauseState)
+        {
+            paused = DFConnection.Instance.DfPauseState;
+            playButton.SetActive(paused);
+            pauseButton.SetActive(!paused);
+        }
+    }
+}
