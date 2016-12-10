bf24f4bf21cef9f1618025a952499c14bdb0ae34
Japa <japa.mala.illo@gmail.com>
2016-12-10 18:47:18 +0530

    Made a slider to change the shape of generated voxels.

---
+++ Assets/MapGen/UserSettings/VoxelShape.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UserSettings
+{
+    public class VoxelShape : SliderBase
+    {
+        protected override void InitValue()
+        {
+            slider.value = (float)GameSettings.Instance.meshing.cornerType;
+            OnValueChanged(slider.value);
+        }
+
+        protected override void OnValueChanged(float value)
+        {
+            GameSettings.Instance.meshing.cornerType = (VoxelGenerator.CornerType)value;
+            valueLabel.text = GameSettings.Instance.meshing.cornerType.ToString();
+            gameMap.Refresh();
+        }
+    }
+}
