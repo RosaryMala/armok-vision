a0e41fead61ad47a3232b228a1421c1e9671c552
Japa <japa.mala.illo@gmail.com>
2016-12-13 09:46:01 +0530

    Started a material manager.

---
+++ Assets/MapGen/MaterialManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialManager : MonoBehaviour
+{
+    [Flags]
+    public enum MaterialFlags
+    {
+        None = 0,
+        Contaminants = 1
+    }
+    public enum TransparencyType
+    {
+        Opaque,
+        Stencil,
+        Transparent
+    }
+
+    [SerializeField]
+    Material baseStandardMaterial;
+
+    [SerializeField]
+    Material baseSplatMaterial;
+}
