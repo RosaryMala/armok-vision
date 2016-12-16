c4656c016f593a677a76f6e1db09974c10a95f8a
Japa Illo <japa.mala.illo@gmail.com>
2016-12-16 15:14:08 +0530

    Fix post-build function for cloud-build compatibility.

--- Assets/Editor/BuildPipeline.cs
+++ Assets/Editor/BuildPipeline.cs
@@ -58,7 +58,6 @@ public class BuildFactory
         EditorUserBuildSettings.SetPlatformSettings("Standalone", "CopyPDBFiles", "false");
         Debug.Log(BuildPipeline.BuildPlayer(levels, path + releaseName, target, BuildOptions.None));
         CopyExtras(path);
-        File.WriteAllText(path + "manifest.json", JsonConvert.SerializeObject(buildSettings, Formatting.Indented));
 
         using (ZipFile zip = new ZipFile())
         {
@@ -69,9 +68,12 @@ public class BuildFactory
 
     static void CopyExtras(string path)
     {
+        path = Path.GetDirectoryName(path) + "/";
+        BuildSettings buildSettings = Resources.Load("Build Settings", typeof(BuildSettings)) as BuildSettings;
         FileUtil.ReplaceFile("ReleaseFiles/Changelog.txt", path + "Changelog.txt");
         FileUtil.ReplaceFile("ReleaseFiles/Credits.txt", path + "Credits.txt");
         FileUtil.ReplaceFile("ReleaseFiles/Readme.txt", path + "Readme.txt");
         FileUtil.ReplaceDirectory("ReleaseFiles/Plugins/", path + "Plugins");
+        File.WriteAllText(path + "manifest.json", JsonConvert.SerializeObject(buildSettings, Formatting.Indented));
     }
 }
