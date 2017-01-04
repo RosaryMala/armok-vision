fd77532ff3194fb3a6b3d0d62881403599d9900e
Japa Illo <japa.mala.illo@gmail.com>
2017-01-04 12:51:03 +0530

    Create a basic console script. Not finished yet.

---
+++ Assets/UIScripts/OnscreenConsole.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnscreenConsole : MonoBehaviour
+{
+    Queue logQueue = Queue.Synchronized(new Queue());
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
