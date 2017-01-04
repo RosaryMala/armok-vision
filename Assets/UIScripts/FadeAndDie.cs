add2215e5feb771e059e6cbcb44f90f033024208
Japa Illo <japa.mala.illo@gmail.com>
2017-01-04 12:50:40 +0530

    Add a function to fade out and remove canvas elements

---
+++ Assets/UIScripts/FadeAndDie.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CanvasRenderer))]
+public class FadeAndDie : MonoBehaviour
+{
+
+    float startTime;
+    public float startfade;
+    public float fadelength;
+
+    CanvasRenderer canRen;
+
+    private void Awake()
+    {
+        canRen = GetComponent<CanvasRenderer>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float relTime = Time.time - startTime;
+        if (relTime > (startfade + fadelength))
+            Destroy(gameObject);
+        else if(relTime > startfade)
+        {
+            canRen.SetAlpha(Mathf.Lerp(1, 0, (relTime - startfade) / fadelength));
+        }
+    }
+}
