1036427b71d133c66125bb928b298d601c07efe6
Japa <japa.mala.illo@gmail.com>
2017-01-25 10:12:51 +0530

    Beginning work on mesh serialization.

---
+++ Assets/MeshSerializer/MeshSerializer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshSerializer : MonoBehaviour
+{
+    public MeshContentSerialized outputMesh;
+
+    private void Start()
+    {
+        outputMesh = new MeshContentSerialized();
+    }
+}
