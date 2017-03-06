8a9394ad83a27d5cf0b0102640501ae3adc8942d
Japa <japa.mala.illo@gmail.com>
2017-03-06 21:42:42 +0530

    Fix the help overlay fade.

--- Assets/MapGen/ContentLoader.cs
+++ Assets/MapGen/ContentLoader.cs
@@ -248,7 +248,7 @@ public class ContentLoader : MonoBehaviour
         Debug.Log("Took a total of " + watch.ElapsedMilliseconds + "ms to load all XML files.");
         Debug.Log(string.Format("loaded {0} meshes, {1} pattern textures, {2} colors, and {3} shape textures.", MeshContent.NumCreated, TextureContent.NumCreated, ColorContent.NumCreated, NormalContent.NumCreated));
         Debug.Log("Loading Complete. Press ESC to change settings or leave feedback. Have a nice day!");
-        StartCoroutine(GameMap.Instance.EnableHelp());
+        GameMap.Instance.ShowHelp();
         yield return null;
     }
 
