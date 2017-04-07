ef6ea47e8f67f36152fe15770e7d61cd1184e5ed
Japa <japa.mala.illo@gmail.com>
2017-04-07 19:00:19 +0530

    Added smoke effect to furnaces

--- Assets/Editor/CrossPlatformInput/CrossPlatformInputInitialize.cs
+++ Assets/Editor/CrossPlatformInput/CrossPlatformInputInitialize.cs
@@ -85,8 +85,7 @@ namespace UnityStandardAssets.CrossPlatformInput.Inspector
             {
                 BuildTargetGroup.Standalone,
                 BuildTargetGroup.Android,
-                BuildTargetGroup.iOS,
-                BuildTargetGroup.WSA
+                BuildTargetGroup.iOS
             };
 
         private static BuildTargetGroup[] mobileBuildTargetGroups = new BuildTargetGroup[]
