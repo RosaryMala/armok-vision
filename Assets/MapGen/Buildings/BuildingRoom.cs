f46ff008e3d01ff9baa2e59fff23e3b15f183629
Japa <japa.mala.illo@gmail.com>
2017-05-10 18:59:58 +0530

    Got bridges, roads, and farm plots properly working.

--- Assets/MapGen/Buildings/BuildingRoom.cs
+++ Assets/MapGen/Buildings/BuildingRoom.cs
@@ -21,16 +21,23 @@ namespace Building
             setParts.Clear();
             var room = buildingInput.room;
             if (room == null || room.extents.Count == 0)
-                return;
-            for (int x = 0; x < room.width; x++)
-                for (int y = 0; y < room.height; y++)
-                {
-                    var set = room.extents[x + y * room.width];
-                    if (set == 0)
-                        continue;
-                    GameObject newPart = Instantiate(part, GameMap.DFtoUnityCoord(room.pos_x + x, room.pos_y + y, buildingInput.pos_z_max), Quaternion.identity, transform);
-                    setParts.Add(newPart);
-                }
+            {
+                for(int x = buildingInput.pos_x_min; x <= buildingInput.pos_x_max; x++)
+                    for(int y = buildingInput.pos_y_min; y <= buildingInput.pos_y_max; y++)
+                    {
+                        GameObject newPart = Instantiate(part, GameMap.DFtoUnityCoord(x, y, buildingInput.pos_z_max), Quaternion.identity, transform);
+                        setParts.Add(newPart);
+                    }
+            }
+            else for (int x = 0; x < room.width; x++)
+                    for (int y = 0; y < room.height; y++)
+                    {
+                        var set = room.extents[x + y * room.width];
+                        if (set == 0)
+                            continue;
+                        GameObject newPart = Instantiate(part, GameMap.DFtoUnityCoord(room.pos_x + x, room.pos_y + y, buildingInput.pos_z_max), Quaternion.identity, transform);
+                        setParts.Add(newPart);
+                    }
         }
     }
 }
