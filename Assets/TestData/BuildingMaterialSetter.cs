b1b1c0369658bc92b7d622c9a23638cc5238e0b8
Japa Illo <japa.mala.illo@gmail.com>
2017-05-22 16:33:42 +0530

    Fixed broken workshops from the model file rename.

---
+++ Assets/TestData/BuildingMaterialSetter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingMaterialSetter : MonoBehaviour
+{
+    public Color materialColor;
+    public Texture2DArray materialPattern;
+    public int index;
+
+    public void SetMaterials()
+    {
+        foreach (var part in FindObjectsOfType<Building.BuildingPart>())
+        {
+            var renderer = part.GetComponent<MeshRenderer>();
+            renderer.sharedMaterial.SetTexture("_MatTex", materialPattern);
+            MaterialPropertyBlock prop = new MaterialPropertyBlock();
+            prop.SetColor("_MatColor", materialColor.gamma);
+            prop.SetFloat("_MatIndex", index);
+            renderer.SetPropertyBlock(prop);
+        }
+    }
+
+    private void OnValidate()
+    {
+        SetMaterials();
+    }
+}
