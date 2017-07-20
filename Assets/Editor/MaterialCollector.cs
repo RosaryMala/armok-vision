8a61e34ec38a223301c136e04798c85437cfaf70
Japa Illo <japa.mala.illo@gmail.com>
2017-07-20 16:02:22 +0530

    Finished making the pattern texture array. Todo: make the other texture arrays.

--- Assets/Editor/MaterialCollector.cs
+++ Assets/Editor/MaterialCollector.cs
@@ -22,7 +22,11 @@ namespace MaterialStore
             List<MaterialTextureSet> materialList = new List<MaterialTextureSet>();
 
             Dictionary<string, int> patternIndex = new Dictionary<string, int>();
-            List<Texture2D> patternList = new List<Texture2D>();
+            List<Texture2D> albedoList = new List<Texture2D>();
+            List<Texture2D> specularList = new List<Texture2D>();
+
+            var defaultAlbedo = Resources.Load<Texture2D>("Grey");
+            var defaultSpecular = Resources.Load<Texture2D>("Low_S");
 
             foreach (var item in guids)
             {
@@ -32,11 +36,54 @@ namespace MaterialStore
 
                 set.tag = MaterialTag.Parse(mat.name);
 
-                Debug.Log(set.tag);
-
                 Texture2D albedo = (Texture2D)mat.GetTexture(albedoID);
                 Texture2D specular = (Texture2D)mat.GetTexture(specularID);
+
+                if (albedo == null)
+                    albedo = defaultAlbedo;
+
+                if (specular == null)
+                    specular = defaultSpecular;
+
+                string id = albedo.GetInstanceID().ToString() + "," + specular.GetInstanceID().ToString();
+
+                if(patternIndex.ContainsKey(id))
+                {
+                    set.patternIndex = patternIndex[id];
+                }
+                else
+                {
+                    set.patternIndex = albedoList.Count;
+                    patternIndex[id] = albedoList.Count;
+                    albedoList.Add(albedo);
+                    specularList.Add(specular);
+                }
             }
+
+            Texture2DArray patternArray = new Texture2DArray(256, 256, albedoList.Count, TextureFormat.ARGB32, true, false);
+            Texture2D tempTex = new Texture2D(256, 256, TextureFormat.ARGB32, false);
+            var patternColors = patternArray.GetPixels(0);
+            Material patternMat = new Material(Shader.Find("Hidden/PatternTextureMaker"));
+
+            for (int i = 0; i < albedoList.Count; i++)
+            {
+                Debug.Log(albedoList[i].name + "," + specularList[i].name);
+                var albedoTarget = RenderTexture.GetTemporary(256, 256, 0, RenderTextureFormat.ARGB32);
+                patternMat.SetTexture(specularID, specularList[i]);
+                Graphics.Blit(albedoList[i], albedoTarget, patternMat);
+
+                var backup = RenderTexture.active;
+                RenderTexture.active = albedoTarget;
+                tempTex.ReadPixels(new Rect(0, 0, 256, 256), 0, 0);
+                patternArray.SetPixels(tempTex.GetPixels(), i);
+                RenderTexture.active = backup;
+                RenderTexture.ReleaseTemporary(albedoTarget);
+            }
+
+            patternArray.Apply(true);
+
+            AssetDatabase.CreateAsset(patternArray, "Assets/Resources/patternTextures.asset");
+            AssetDatabase.SaveAssets();
         }
     }
 }
---
+++ Assets/Shaders/PatternTextureMaker.shader
@@ -0,0 +1,54 @@
+﻿Shader "Hidden/PatternTextureMaker"
+{
+	Properties
+	{
+        _MainTex("Albedo (RGB)", 2D) = "grey" {}
+        _Specular("specular (R)", 2D) = "grey" {}
+    }
+	SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+			
+			sampler2D _MainTex;
+            sampler2D _Specular;
+
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 col = tex2D(_MainTex, i.uv);
+				// just invert the colors
+				col.a = tex2D(_MainTex, i.uv).r;
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
