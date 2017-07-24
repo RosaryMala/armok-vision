420913c5204f8b8407928519435c4c6a1f0fa518
Japa <japa.mala.illo@gmail.com>
2017-07-24 09:50:57 +0530

    Add construction shape materials.

---
+++ Assets/MapGen/MaterialStore/BuiltItemTextureSet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MaterialStore
+{
+    [System.Serializable]
+    public class BuiltItemTextureSet
+    {
+        public string token;
+        public int shapeIndex;
+    }
+}
