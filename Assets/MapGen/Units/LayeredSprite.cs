b66eb98d2586b83bb388301acb16b3ff36cd004e
Japa Illo <japa.mala.illo@gmail.com>
2017-08-10 15:31:28 +0530

    Sprite layer maker

---
+++ Assets/MapGen/Units/LayeredSprite.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayeredSprite : MonoBehaviour
+{
+    public float gap;
+    public CreatureSpriteCollection spriteCollection;
+    private void Start()
+    {
+        BuildSpriteLayers(spriteCollection.spriteLayers);
+    }
+
+    private void BuildSpriteLayers(List<CreatureSpriteLayer> spriteLayers)
+    {
+        float depth = 0;
+        foreach (var layer in spriteLayers)
+        {
+            if (!layer.preview)
+                continue;
+            GameObject go = new GameObject(layer.spriteTexture.name);
+            go.transform.SetParent(transform);
+            var sr = go.AddComponent<SpriteRenderer>();
+            sr.color = layer.color;
+            sr.sprite = layer.spriteTexture;
+            var pos = go.transform.localPosition;
+            pos.x = depth;
+            go.transform.position = pos;
+            depth -= gap;
+        }
+    }
+}
