c7cb452c4b86990fdc23a1532f218c3dc7093120
Japa <japa.mala.illo@gmail.com>
2018-02-06 10:46:46 +0530

    Made a tool to make all sprites in a dir opaque

---
+++ Assets/Editor/SpriteFixer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class SpriteFixer
+{
+    [MenuItem("Mytools/Fix Semitransparent Sprites")]
+    public static void FixTransparentTextures()
+    {
+        var path = EditorUtility.OpenFolderPanel("Pick Sprite Folder", "", "SpriteFolder");
+        var files = Directory.GetFiles(path, "*.png");
+        foreach (var file in files)
+        {
+            Texture2D tex = new Texture2D(2, 2, TextureFormat.ARGB32, false, true);
+            tex.LoadImage(File.ReadAllBytes(file));
+            var pixels = tex.GetPixels();
+            float maxAlpha = 0;
+            for(int i = 0; i < pixels.Length; i++)
+            {
+                maxAlpha = Mathf.Max(pixels[i].a, maxAlpha);
+            }
+            for (int i = 0; i < pixels.Length; i++)
+            {
+                pixels[i].a /= maxAlpha;
+            }
+            tex.SetPixels(pixels);
+            File.WriteAllBytes(file, tex.EncodeToPNG());
+        }
+    }
+}
