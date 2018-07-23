b63a560a4e700843d4f0f55ca3486144c206d2ae
Japa Illo <japa.mala.illo@gmail.com>
2018-07-23 13:18:18 +0530

    Begin modeling a more controllable part class.

---
+++ Assets/Scripts/UnitCreator/BodyPartModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyPartModel : MonoBehaviour
+{
+    [System.Serializable]
+    public class ChildLocation
+    {
+        public string category;
+        public string token;
+        public Vector3 pos1;
+        public Vector3 pos2;
+        public Vector3 posDefault;
+        public Vector3 rot1;
+        public Vector3 rot2;
+        public Vector3 rotDefault;
+    }
+
+
+    public float canonVolume = 1;
+    public List<ChildLocation> childParts = new List<ChildLocation>();
+}
