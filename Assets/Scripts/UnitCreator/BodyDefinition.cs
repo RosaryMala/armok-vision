fab5e132926bb7af663bca13bddb84208fbf5b76
Japa Illo <japa.mala.illo@gmail.com>
2018-07-26 13:07:25 +0530

    Made a container for body parts

---
+++ Assets/Scripts/UnitCreator/BodyDefinition.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class BodyDefinition : ScriptableObject
+{
+    [System.Serializable]
+    public class BodyPartSelection
+    {
+        public string category;
+        public bool categoryRegex;
+        public string token;
+        public bool tokenRegex;
+        public BodyPartModel model;
+    }
+    public List<BodyPartSelection> bodyParts;
+}
