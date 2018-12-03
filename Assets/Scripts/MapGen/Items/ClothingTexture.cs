5a809f5a73e1abea5a3e974ea2c3167f184b540f
JapaMala <japa.mala.illo@gmail.com>
2018-12-02 21:37:49 -0600

    Added models for a new generic clothing model system. Not used yet.

---
+++ Assets/Scripts/MapGen/Items/ClothingTexture.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class ClothingTexture : ScriptableObject
+{
+    public bool isDress;
+    public Texture2D texture;
+    public Texture2D mask;
+    public Texture2D bump;
+}
---
+++ Assets/Scripts/MapGen/Items/GenericClothingItem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class GenericClothingItem : MonoBehaviour
+{
+    public enum DressUsage
+    {
+        Any,
+        DressOnly,
+        NotDress
+    }
+    public DressUsage isDress = DressUsage.Any;
+}
