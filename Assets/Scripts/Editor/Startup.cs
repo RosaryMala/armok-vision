e3a2105efa7f963b459325bf62ddc8ac8630f5a2
Rose <japa.mala.illo@gmail.com>
2020-09-02 00:09:17 -0700

    Convert building shader to shadergraph

---
+++ Assets/Scripts/Editor/Startup.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class Startup
+{
+    static Startup()
+    {
+        Shader.SetGlobalVector("_ViewMin", new Vector3(-9999, -9999, -9999));
+        Shader.SetGlobalVector("_ViewMax", new Vector3(9999, 9999, 9999));
+    }
+}
